60ceb8d288783c3b262e36e5d2df4e6749a9718d
Marc Perrin-Pelletier <marcp@forestadmin.com>
2019-12-18 10:29:10 +0100

    Test docker in production

---
+++ routes/generictwos.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { generictwos } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('generictwos');
+
+// This file contains the logic of every route in Forest Admin for the collection generictwos:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Generictwo
+router.post('/generictwos', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Generictwo
+router.put('/generictwos/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Generictwo
+router.delete('/generictwos/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Generictwos
+router.get('/generictwos', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Generictwos
+router.get('/generictwos/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Generictwo
+router.get('/generictwos/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Generictwos
+router.get('/generictwos.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v5/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+module.exports = router;
